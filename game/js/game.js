a0a239ecac2c71d38c47c7eef516969b5eeac1b4
Sylvain Meyer <sylvain.meyer@hes-so.ch>
2020-05-03 10:37:24 +0200

    Début du Drap & Drop

--- game/js/game.js
+++ game/js/game.js
@@ -3,11 +3,49 @@ var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 967;
 canvas.height = 579;
-//canvas.width = 512;
-//canvas.height = 480;
 var imgData = ctx.getImageData(0,0,canvas.width,canvas.height);
-//document.body.appendChild(canvas);
 document.getElementById("jeu").appendChild(canvas);
+
+var BB = canvas.getBoundingClientRect();
+var offsetX = BB.left;
+var offsetY = BB.top;
+var WIDTH = canvas.width;
+var HEIGHT = canvas.height;
+
+// drag related variables
+var dragok = false;
+var startX;
+var startY;
+
+// an array of objects that define different rectangles
+var rects = [];
+rects.push({
+    x: 75 - 45,
+    y: 50 - 45,
+    width: 30,
+    height: 30,
+    fill: "#0c64e8",
+    isDragging: false
+});
+
+// listen for mouse events
+canvas.onmousedown = myDown;
+canvas.onmouseup = myUp;
+canvas.onmousemove = myMove;
+
+
+// draw a single rect
+function rect(x, y, w, h) {
+    ctx.beginPath();
+    ctx.rect(x, y, w, h);
+    ctx.closePath();
+    ctx.fill();
+}
+
+// clear the canvas
+function clear() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
 
 // Background image
 var bgReady = false;
@@ -115,6 +153,16 @@ var render = function () {
 		ctx.drawImage(monsterImage, monster.x, monster.y);
 	}
 
+    //clear();
+    //ctx.fillStyle = "#FAF7F8";
+    //rect(0, 0, WIDTH, HEIGHT);
+    // redraw each rect in the rects[] array
+    for (var i = 0; i < rects.length; i++) {
+        var r = rects[i];
+        ctx.fillStyle = r.fill;
+        rect(r.x, r.y, r.width, r.height);
+    }
+
 	// Score
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "24px Helvetica";
@@ -124,7 +172,6 @@ var render = function () {
 };
 
 // Sylvain : border checking function
-
 function isInMap(position,limit) {
     var x = hero.x;
     var y = hero.y;
@@ -137,13 +184,93 @@ function isInMap(position,limit) {
 
     var data = ctx.getImageData(x, y, canvas.width, canvas.height).data;
     var rgb = [ data[0], data[1], data[2] ];
-    console.log(rgb);
+    //console.log(rgb);
     if (data[0] === 255 && data[1] === 51 && data[2] === 51){
         return false;
     } else {
         return true;
     }
 
+}
+
+// handle mousedown events
+function myDown(e) {
+
+    // tell the browser we're handling this mouse event
+    e.preventDefault();
+    e.stopPropagation();
+
+    // get the current mouse position
+    var mx = parseInt(e.clientX - offsetX);
+    var my = parseInt(e.clientY - offsetY);
+
+    // test each rect to see if mouse is inside
+    dragok = false;
+    for (var i = 0; i < rects.length; i++) {
+        var r = rects[i];
+        if (mx > r.x && mx < r.x + r.width && my > r.y && my < r.y + r.height) {
+            // if yes, set that rects isDragging=true
+            dragok = true;
+            r.isDragging = true;
+        }
+    }
+    // save the current mouse position
+    startX = mx;
+    startY = my;
+}
+
+
+// handle mouseup events
+function myUp(e) {
+    // tell the browser we're handling this mouse event
+    e.preventDefault();
+    e.stopPropagation();
+
+    // clear all the dragging flags
+    dragok = false;
+    for (var i = 0; i < rects.length; i++) {
+        rects[i].isDragging = false;
+    }
+}
+
+
+// handle mouse moves
+function myMove(e) {
+    // if we're dragging anything...
+    if (dragok) {
+
+        // tell the browser we're handling this mouse event
+        e.preventDefault();
+        e.stopPropagation();
+
+        // get the current mouse position
+        var mx = parseInt(e.clientX - offsetX);
+        var my = parseInt(e.clientY - offsetY);
+
+        // calculate the distance the mouse has moved
+        // since the last mousemove
+        var dx = mx - startX;
+        var dy = my - startY;
+
+        // move each rect that isDragging
+        // by the distance the mouse has moved
+        // since the last mousemove
+        for (var i = 0; i < rects.length; i++) {
+            var r = rects[i];
+            if (r.isDragging) {
+                r.x += dx;
+                r.y += dy;
+            }
+        }
+
+        // redraw the scene with the new rect positions
+        render();
+
+        // reset the starting mouse position for the next mousemove
+        startX = mx;
+        startY = my;
+
+    }
 }
 
 
